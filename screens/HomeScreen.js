ea6ea43f105a05acf21f365bdfd1856925bb7629
Brian Zhang <brian.w.zhang@gmail.com>
2024-03-30 20:05:02 -0400

    add food placeholder boxes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,24 @@
-// LoginScreen.js, HomeScreen.js, RecipeScreen.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import BackgroundImage from '../components/Background'; // Import the BackgroundImage component
+import { View, StyleSheet, Dimensions } from 'react-native';
+import BackgroundImage from '../components/Background';
 
 const HomeScreen = () => {
+  const screenWidth = Dimensions.get('window').width; // Get the screen width
+
   return (
-    <BackgroundImage backgroundImage={require('../assets/fridge.jpg')}>
+    <BackgroundImage backgroundImage={require('../assets/pantry2.png')}>
       <View style={styles.container}>
-        <Text>Home Screen</Text>
+        <View style={styles.outerGrid}>
+          {[...Array(4).keys()].map((row) => (
+            <View key={row} style={styles.row}>
+              <View style={styles.innerGrid}>
+                {[...Array(4).keys()].map((col) => (
+                  <View key={col} style={[styles.gridItem]} />
+                ))}
+              </View>
+            </View>
+          ))}
+        </View>
       </View>
     </BackgroundImage>
   );
@@ -15,10 +26,27 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    alignItems: 'center', // Center horizontally
+    paddingBottom: '63%', // Adjust bottom position as needed
+  },
+  outerGrid: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  innerGrid: {
+    flexDirection: 'row',
+  },
+  gridItem: {
+    backgroundColor: 'red',
+    marginHorizontal: 13,
+    height: 60,
+    width: 60, // Set width as percentage (4 items in a row with 2% margin)
+    marginTop: 72.5,
   },
 });
-
 export default HomeScreen;
